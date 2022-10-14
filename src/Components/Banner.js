582813de6f64263adcca123bab111070b0ce0007
iksagor0 <iksagor0@gmail.com>
2022-10-14 10:29:52 +0600

    add Banner Text with type-writter

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
 import BannerTwo from "../Assets/images/banner-1.jpg";
 import BannerOne from "../Assets/images/banner-2.jpg";
 import BannerThree from "../Assets/images/banner-3.jpg";
@@ -9,7 +10,7 @@ import { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
+// import "swiper/css/pagination";
 
 export default function Banner() {
     return (
@@ -36,6 +37,23 @@ export default function Banner() {
                         <img src={BannerFour} alt="banner-img" />
                     </SwiperSlide>
                 </Swiper>
+            </div>
+            <div className="banner_heading">
+                <h1>
+                    <Typewriter
+                        words={[
+                            "আপনাদের দোয়া ও ভালবাসায় জনগনের সেবায় নিবেদিত প্রাণ",
+                        ]}
+                        loop={false}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={100}
+                        deleteSpeed={60}
+                        delaySpeed={5000}
+                    />
+                </h1>
+                {/* <h1>আপনাদের দোয়া ও ভালবাসায় জনগনের সেবায় নিবেদিত প্রাণ </h1> */}
+                <button className="btn">যোগাযোগ করুন</button>
             </div>
         </div>
     );
