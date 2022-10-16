5d29bcae00ea74a5525fad4fcdab8aefa9c35114
iksagor0 <iksagor0@gmail.com>
2022-10-16 15:01:44 +0600

    edit LOGO

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Boat from "../Assets/images/Boat.svg";
 import LOGO from "../Assets/images/LOGO.svg";
 
@@ -7,8 +8,9 @@ export default function Nav() {
         <nav id="nav">
             <div className="container">
                 <div className="logo_container">
-                    <span>K</span>
-                    <img src={LOGO} alt="Logo" />
+                    <Link to={"/"}>
+                        <img src={LOGO} alt="Logo" />
+                    </Link>
                 </div>
                 <div className="boat_area">
                     <img src={Boat} alt="Boat" />
