933a3d5419175278d40c6aeda20d5b2fee6d2616
iksagor0 <iksagor0@gmail.com>
2022-10-18 12:51:09 +0600

    fix banner heading issue AND add sticky on nav

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Boat from "../Assets/images/Boat.svg";
 import LOGO from "../Assets/images/LOGO.svg";
 
 export default function Nav() {
+    const [sticky, setSticky] = useState(false);
+    useEffect(() => {
+        // console.log(window.scrollBy(0, 1000));
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 500) {
+                console.log(window.scrollY);
+                console.log(sticky);
+                setSticky(true);
+            } else {
+                setSticky(false);
+            }
+        });
+    }, [sticky]);
+
     return (
-        <nav id="nav">
+        <nav id="nav" className={sticky && "sticky"}>
             <div className="container">
                 <div className="logo_container">
                     <Link to={"/"}>
