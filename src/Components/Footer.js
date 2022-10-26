091dbaa7a6531a3ea9dadeab7b3a935cb0606504
iksagor0 <iksagor0@gmail.com>
2022-10-26 15:35:57 +0600

    responsive & fix Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className="container">
                 <div className="copyrights">
                     <span>Copyright © 2022</span>
-                    <h6>| কোহেলী কুদ্দুস মুক্তি</h6>
+                    <h6 className="bn">| কোহেলী কুদ্দুস মুক্তি</h6>
                 </div>
                 <div className="rights">All rights reserved!</div>
             </div>
