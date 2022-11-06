0f056fd27ab3b66466601110c331985c7982d1b2
iksagor0 <iksagor0@gmail.com>
2022-11-06 08:42:40 +0600

    bind data in Introduction

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 import Banner from "../Components/Banner";
 import Cards from "../Components/Cards";
@@ -10,18 +11,36 @@ import SocialWorks from "../Components/SocialWorks";
 import Strength from "../Components/Strength";
 
 export default function Home() {
+  const [allData, setAlldata] = React.useState(null);
+
+  async function fetchAboutData() {
+    try {
+      const response = await axios.get(
+        "http://koheli.sscquizcontest.com/api/home/page-data"
+      );
+      setAlldata(response.data.data);
+      console.log(allData);
+    } catch (error) {
+      //
+    }
+  }
+
+  React.useEffect(() => {
+    fetchAboutData();
+  }, [allData]);
+
   return (
     <>
       <Banner />
       <div className="container">
         <Cards />
-        <Introduction />
-        <Strength />
+        <Introduction allData={allData} />
+        <Strength allData={allData} />
       </div>
-      <Programs />
+      <Programs allData={allData} />
       <div className="container">
-        <PoliticalTour />
-        <SocialWorks />
+        <PoliticalTour allData={allData} />
+        <SocialWorks allData={allData} />
         <PhotosVideos />
         <ContactMe />
       </div>
